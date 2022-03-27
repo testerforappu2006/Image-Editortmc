f690083b77096c7f81793ee540c5e045fa9485be
testerforappu2006 <94781974+testerforappu2006@users.noreply.github.com>
2022-03-27 20:46:08 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -10,7 +10,6 @@ async def start(client: Client, message: Message):
         await message.reply_photo(
             photo="https://telegra.ph/file/6aa785598aded597f1f7c.png"
             caption=script.START_MSG.format(message.from_user.mention),
-            disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
