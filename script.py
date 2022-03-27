a1f974400fc4f8c1096fc4e6a92d52b1c6c962c7
testerforappu2006 <94781974+testerforappu2006@users.noreply.github.com>
2022-03-27 18:17:42 +0530

    Update script.py

--- script.py
+++ script.py
@@ -7,20 +7,15 @@ I'm a Image Editor Bot which Supports various modes
 For more click help....</b>"""
     HELP_MSG = """Hai, Follow these Steps..
 
-<code>🌀 Send me any Image to Edit..</code>
+<b>🌀 Send me any Image to Edit..<b>
 
-<code>🌀 Select the Corresponding mode that you need</code>
+<b>🌀 Select the Corresponding mode that you need<b>
 
-<code>🌀 Your Edited Image will be Uploaded When Done</code>
-
-
-© @TMC_BOTX"""
+<b>🌀 Your Edited Image will be Uploaded When Done<b>"""
     ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
 
-⭕️<b>Owner :</b> 👉 <a href='https://t.me/TMC_BOTX'>Click Here</a>
-
-⭕️<b>Support :</b> 👉 <a href='https://t.me/TMC_BOTX_Group'>Click Here</a>"""
+⭕️<b>Owner :</b> 👉 <a href='https://t.me/MRToxicDragon'>Akash rapheal</a>"""
